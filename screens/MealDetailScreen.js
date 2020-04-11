4448e789e60a802f5dc295d92e8e9d53b8e592a6
Oksana Danylenko <danylenko.ok@gmail.com>
2020-04-11 23:49:22 +0300

    add meal screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import { MEALS } from '../data/dummy-data';
 
 const MealDetailScreen = props => {
+  const mealId = props.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
   return (
     <View style={styles.screen}>
       <Text>The Meal Details Screen!</Text>
+      <Text>{selectedMeal.title}</Text>
       <Button
         title="Go back to Categories"
         onPress={() => {
@@ -13,6 +18,17 @@ const MealDetailScreen = props => {
       />
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = navigationData => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  console.log('mealId', mealId);
+
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+  return {
+    headerTitle: selectedMeal.title,
+  };
 };
 
 const styles = StyleSheet.create({
